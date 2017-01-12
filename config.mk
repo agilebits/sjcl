42d992819fab4fffde80f1f61c103388ec8d8a87
Rob Yoder <rob@robyoder.com>
2017-01-12 00:58:16 -0700

    Rebuild with b5 config

--- config.mk
+++ config.mk
@@ -1,2 +1,2 @@
-SOURCES= core/sjcl.js core/bn.js core/aes.js core/bitArray.js core/codecBytes.js core/codecString.js core/codecHex.js core/codecBase32.js core/codecBase64.js core/sha256.js core/sha1.js core/sha512.js core/gcm.js core/hmac.js core/pbkdf2.js core/convenience.js core/ecc.js core/random.js
+SOURCES= core/sjcl.js core/exports.js core/bn.js core/aes.js core/bitArray.js core/codecBytes.js core/codecString.js core/codecHex.js core/codecBase32.js core/codecBase64.js core/sha256.js core/sha1.js core/sha512.js core/gcm.js core/hmac.js core/pbkdf2.js core/convenience.js core/ecc.js core/random.js
 COMPRESS= core_closure.js
